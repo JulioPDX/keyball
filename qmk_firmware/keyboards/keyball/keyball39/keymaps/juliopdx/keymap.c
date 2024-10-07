0637e4205b7ee7b5b290af5944a95c64072ba7c0
JulioPDX <juliop503@gmail.com>
2024-10-07 12:07:27 -0700

    add reset option

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/juliopdx/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/juliopdx/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LT(3, KC_LGUI),LT(1, KC_TAB), LT(4, KC_BSPC),SFT_T(KC_SPC),LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, LT(4, KC_RSFT)
+    KC_LCTL  , KC_LGUI  , KC_LALT  ,LT(3, KC_LGUI),LT(1, KC_TAB), LT(4, KC_BSPC),SFT_T(KC_SPC),LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, LT(4, KC_DEL)
   ),
 
   [1] = LAYOUT_universal(
